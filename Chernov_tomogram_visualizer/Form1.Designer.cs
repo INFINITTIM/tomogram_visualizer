9c30d4e4fd3255b8a9415b3f0de2ef636dca7b6e
TimV <www.tiimchernov@mail.ru>
2025-03-27 11:17:13 +0300

    second commit

--- Chernov_tomogram_visualizer/Form1.Designer.cs
+++ Chernov_tomogram_visualizer/Form1.Designer.cs
@@ -37,6 +37,8 @@
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.trackBar2 = new System.Windows.Forms.TrackBar();
             this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
@@ -114,30 +116,52 @@
             // 
             // trackBar2
             // 
-            this.trackBar2.Location = new System.Drawing.Point(12, 607);
+            this.trackBar2.Location = new System.Drawing.Point(90, 607);
             this.trackBar2.Maximum = 255;
             this.trackBar2.Name = "trackBar2";
-            this.trackBar2.Size = new System.Drawing.Size(430, 56);
+            this.trackBar2.Size = new System.Drawing.Size(396, 56);
             this.trackBar2.TabIndex = 5;
             this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
             // 
             // trackBar3
             // 
-            this.trackBar3.Location = new System.Drawing.Point(492, 607);
+            this.trackBar3.Location = new System.Drawing.Point(625, 607);
             this.trackBar3.Maximum = 255;
             this.trackBar3.Minimum = 1;
             this.trackBar3.Name = "trackBar3";
             this.trackBar3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.trackBar3.Size = new System.Drawing.Size(483, 56);
+            this.trackBar3.Size = new System.Drawing.Size(421, 56);
             this.trackBar3.TabIndex = 6;
             this.trackBar3.Value = 255;
             this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 607);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 39);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "min";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(522, 607);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 39);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "width";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1058, 760);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.trackBar3);
             this.Controls.Add(this.trackBar2);
             this.Controls.Add(this.checkBox2);
@@ -169,6 +193,8 @@
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.TrackBar trackBar2;
         private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
